0120afe6c9deb608a6e28f46bdc6fd300412d1d5
xiaolin <462441355@qq.com>
2019-05-05 10:06:59 +0800

    * token测试

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Support\Encrypt\Clients\Encryption;
+use App\Support\Utility\JwtAuth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -23,5 +24,12 @@ class ExampleTest extends TestCase
         $str = Encryption::getInstance()->encrypt("ceshi");
         $pwd = Encryption::getInstance()->decrypt($str);
         $this->assertStringEndsWith($pwd, "ceshi");
+    }
+
+    public function testToken()
+    {
+        $token = JwtAuth::getToken(['userId' => 1024]);
+        $verify = JwtAuth::verifyToken($token);
+        $this->assertEquals(array_get($verify, 'userId'), 1024);
     }
 }
